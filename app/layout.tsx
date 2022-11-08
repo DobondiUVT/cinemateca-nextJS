b32c22938afca035225d5296c143d784cdee32e1
DobDaniel <david.dobondi@moisenicoaraonline.ro>
2022-11-08 20:29:56 +0200

    configured tailwind, eslint, prettier, next13 layout

---
+++ app/layout.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @next/next/no-head-element */
+import "./globals.css"
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <head></head>
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <div className="text-[64px] text-blue-700 bold">Testing</div>
+    </div>
+  )
+}
