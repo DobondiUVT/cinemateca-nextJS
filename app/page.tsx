45a3acde02c48e89ace9ee5921df0a07190d07be
DobondiUVT <laszlo.dobondi02@e-uvt.ro>
2022-11-26 20:59:21 +0200

    import fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
 import Benefits from "../components/Benefits"
 import Hero from "../components/Hero"
-import MoviesList from "../components/MoviesList"
-import Navbar from "../components/Navbar"
-import { getMostPopularMovies } from "../helpers/fetch"
 
 export default async function Page() {
-    // const data = await getMostPopularMovies()
-    // <MoviesList movies={data} />
     return (
         <div>
             <Hero />
